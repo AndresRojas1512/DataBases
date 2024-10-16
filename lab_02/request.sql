8ef800847f90f9d5f0e07a9d09c1073857bd6a3e
AndresRojas1512 <andresrojasfcc@gmail.com>
2024-10-16 11:35:52 +0300

    02 between predicate

--- lab_02/request.sql
+++ lab_02/request.sql
@@ -1,4 +1,4 @@
--- 1 Comparision predicates
+-- 1 comparision predicate
 SELECT
     C.model_name,
     C.model_year,
@@ -13,4 +13,18 @@ WHERE
 ORDER BY
     M.manufacturer_name, C.model_year;
 
--- 2
+-- 2 between predicate
+SELECT DISTINCT
+    sale_id,
+    sell_date,
+    dealer_id,
+    car_id,
+    price
+FROM
+    sales
+WHERE
+    sell_date BETWEEN '2023-01-01' AND '2023-03-31'
+ORDER BY
+    sell_date;
+
+-- 3
