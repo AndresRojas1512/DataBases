076691bc6c0cc65d4f747cc82ab373d2db8e004b
AndresRojas1512 <andresrojasfcc@gmail.com>
2024-10-16 11:46:15 +0300

    02 add test records

---
+++ lab_02/insert.sql
@@ -0,0 +1,7 @@
+-- 3 add test records
+INSERT INTO cars (engine_id, manufacturer_id, model_name, body_type, model_year, model_weight)
+VALUES
+    (1, 1, 'Explorer SUV', 'SUV', 2023, 2100),
+    (2, 2, 'Range Rover SUV', 'SUV', 2023, 2200),
+    (3, 1, 'Patriot SUV', 'SUV', 2022, 2000),
+    (4, 3, 'Urbam Cruiser SUV', 'SUV', 2021, 1900);
