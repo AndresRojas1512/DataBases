00da717c354fcd8a9103ae4e41fc8ba1abc04128
AndresRojas1512 <andresrojasfcc@gmail.com>
2024-10-22 15:26:28 +0300

    04 aggregate function

--- lab_04/clr.sql
+++ lab_04/clr.sql
@@ -1,3 +1,4 @@
+-- scalar function
 DROP FUNCTION IF EXISTS max_sale_price_for_manufacturer;
 
 CREATE OR REPLACE FUNCTION max_sale_price_for_manufacturer(manufacturer_id INT)
@@ -18,3 +19,24 @@ RETURNS INT AS $$
 $$ LANGUAGE plpython3u;
 
 SELECT * FROM max_sale_price_for_manufacturer(3);
+
+-- aggregate function
+DROP FUNCTION IF EXISTS get_avg_hp_to_weight_ratio;
+CREATE OR REPLACE FUNCTION get_avg_hp_to_weight_ratio()
+RETURNS FLOAT AS $$
+    query = """
+    SELECT c.body_type, SUM(e.horsepower) AS total_hp, SUM(c.model_weight) AS total_weight
+    FROM cars c
+    JOIN engines e ON c.engine_id = e.engine_id
+    GROUP BY c.body_type;
+    """
+    results = plpy.execute(query)
+
+    if results:
+        ratios = [row['total_hp'] / row['total_weight'] if row['total_weight'] > 0 else 0 for row in results]
+        return sum(ratios) / len(ratios) if ratios else 0
+    else:
+        return 0
+$$ LANGUAGE plpython3u;
+
+SELECT get_avg_hp_to_weight_ratio() AS avg_hp_to_weight_ratio;
